cb0e4008f5ea7d1f04db351a276df080efe16f5d
towhid7667 <towhidulhq@gmail.com>
2022-11-18 18:26:31 +0600

    First commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BannerHome from '../../Components/BannerHome/BannerHome';
+
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+           <BannerHome></BannerHome>
+            
+        </div>
+    );
+};
+
+export default Home;
