0006120f6322c26117d20bdedb82cd177e7deb7a
towhid7667 <towhidulhq@gmail.com>
2022-11-21 12:58:22 +0600

    Apointment page date added

--- src/Components/AppointmentBanner/AppointmentBanner.js
+++ src/Components/AppointmentBanner/AppointmentBanner.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import imgBanner from '../../assets/images/chair.png'
-
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 const AppointmentBanner = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date())
     return (
         <div className="myhero hero p-10 relative my-1">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row-reverse justify-center items-center">
           <img alt='' src={imgBanner} className="rounded-lg w-full lg:w-1/2 md:w-3/4 shadow-2xl" />
-          <div>
-
+          <div className='lg:mr-10 -ml-3 lg:-ml-0'>
+                <DayPicker
+                 mode="single"
+                 selected={selectedDate}
+                 onSelect={setSelectedDate}
+                ></DayPicker>
+                <p>Selected date: {format(selectedDate, 'PP')}</p>
           </div>
         </div>
       </div>
