c8c8b19deab4e5b87931c79eacd1fa0f92811902
towhid7667 <towhidulhq@gmail.com>
2022-11-23 18:02:38 +0600

    login pagees error message added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,23 +3,32 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const { register, formState: { errors }, handleSubmit } = useForm();
+
+  const handleLogin = data => {
+    console.log(data)
+  }
+
   return (
     <div className="flex h-[500px] lg:w-3/12 w-10/12 md:w-5/12 mx-auto my-20 shadow-xl p-10 justify-center items-center rounded-xl">
       <div className="w-full">
         <h2 className="text-xl font-bold uppercase text-center">Login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <label className="label"><span className="label-text">Email</span></label>
-          <input
-            {...register("email")}
+          <input type='email'
+            {...register("email", { required: "Email Address is required" })} 
+            aria-invalid={errors.email ? "true" : "false"}
             placeholder="email"
             className="input input-bordered w-full"
           />
-           <label className="label"><span className="label-text">Password</span></label>
-          <input type='password' {...register("password")} placeholder="password" className="input input-bordered w-full"/>
-          <label className="label "><span className="label-text hover:text-blue-500 font-semibold"><Link>Forgot Password?</Link></span></label>
+           {errors.email && <p className="text-red-600">{errors.email?.message}</p>}
 
+           <label className="label"><span className="label-text">Password</span></label>
+          <input type='password' {...register("password", { required: "Password is required",minLength : {value : 6, message: 'Password must be 6 character or more'} })} 
+             placeholder="password" className="input input-bordered w-full"/>
+              {errors.password && <p className="text-red-600">{errors.password?.message}</p>}
+          <label className="label hover:text-blue-500"><span className="label-text  font-semibold"><Link>Forgot Password?</Link></span></label>
+         
 
           {/* <p>{data}</p> */}
           <input type="submit" value='SUBMIT' className="input input-bordered w-full bg-accent text-white font-bold mt-3 hover:bg-slate-800"/>
